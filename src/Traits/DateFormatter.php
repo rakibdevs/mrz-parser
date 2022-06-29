ede0247d94dad91434da4169341b321d9362342e
Md. Rakibul Islam <rakib1708@gmail.com>
2022-06-29 09:38:12 +0600

    Validate given date format in MRZ

--- src/Traits/DateFormatter.php
+++ src/Traits/DateFormatter.php
@@ -6,10 +6,39 @@ use DateTime;
 
 trait DateFormatter
 {
-    public function formatDate($date, $format = "Y-m-d")
+    /**
+     * Format date from YYMMDD
+     *
+     * @param string $date
+     * @param string $format
+     *
+     * @return null|string
+     */
+    public function formatDate($date, $format = "Y-m-d"): ?string
     {
-        $dateTime = DateTime::createFromFormat('ymd', $date);
+        if ($this->validateDateFormat($date)) {
 
-        return $dateTime->format($format);
+            $dateTime = DateTime::createFromFormat('ymd', $date);
+
+            return $dateTime->format($format);
+        }
+
+        return null;
+    }
+
+    /**
+     * Validate Date Format YYMMDD
+     * MM range must be 01-12
+     * DD range must be 01-31
+     *
+     * @param string $date
+     * @return bool
+     */
+    public function validateDateFormat(string $date): bool
+    {
+        $month = (int) substr($date, 2, 2);
+        $date = (int) substr($date, 4, 2);
+
+        return $month >= 1 && $month <= 12 && $date >= 1 && $date <= 31;
     }
 }
